eb2670df3977b953eeb0d1b4aa4990366512b5a8
Administrator <nguyenhuynhcongly17092000@gmail.com>
2021-09-14 19:45:10 +0700

    Hoàn thành kiểm thử nhánh repo

--- src/main/java/com/iuh/entity/Student.java
+++ src/main/java/com/iuh/entity/Student.java
@@ -2,12 +2,13 @@ package com.iuh.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Student {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
--- src/test/java/com/iuh/SpringDataJpaDemoApplicationTests.java
+++ src/test/java/com/iuh/SpringDataJpaDemoApplicationTests.java
@@ -1,13 +1,49 @@
 package com.iuh;
 
+import com.iuh.entity.Student;
+import com.iuh.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringDataJpaDemoApplicationTests {
+
+	@Autowired
+	private StudentRepository studentRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void findAll() {
+		List<Student> students = studentRepository.findAll();
+		System.out.println(students);
+	}
+
+	@Test
+	void createStudent() {
+		Student student = new Student();
+		student.setName("Nguyen Long Nhut");
+		student.setAge(21);
+		studentRepository.save(student);
+	}
+
+	@Test
+	void updateStudent() {
+		Student student = new Student();
+		student.setId(4);
+		student.setName("Nguyen Long Phan Quan");
+		student.setAge(21);
+		studentRepository.save(student);
+	}
+
+	@Test
+	void deleteStudent() {
+		// Delete student có id = 4
+		studentRepository.deleteById(4);
+	}
 }
